4cf72f674c7b5f242a167637ba7de298e62e2699
Ilgu Lee <iglee2000ca@gmail.com>
2019-02-07 16:36:38 -0500

    Adjusted the bottom navigation from wizard project

---
+++ app/src/main/java/ilgulee/com/kotlinfdanutricious/MainActivity.kt
@@ -0,0 +1,30 @@
+package ilgulee.com.kotlinfdanutricious
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_my_foods -> {
+                message.setText(R.string.title_my_foods)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
